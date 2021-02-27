fc73d032e3de38da96be2cf5e7bba37f36c5b724
Ganeshram Chockalingam <ganeshramc@live.com>
2021-02-26 23:51:28 -0800

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("js") version "1.4.31"
+}
+
+group = "me.ganeshramc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlin-js-wrappers") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-js"))
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.113-kotlin-1.4.0")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.113-kotlin-1.4.0")
+}
+
+kotlin {
+    js(LEGACY) {
+        browser {
+            binaries.executable()
+            webpackTask {
+                cssSupport.enabled = true
+            }
+            runTask {
+                cssSupport.enabled = true
+            }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
+            }
+        }
+    }
+}
+
+// Heroku Deployment (chapter 9)
+tasks.register("stage") {
+    dependsOn("build")
+}
